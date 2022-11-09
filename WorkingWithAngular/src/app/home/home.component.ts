433f2e4fac3c48b26c0b8f01090abb9901ea2a8f
JECT <eliank500@gmail.com>
2022-11-09 17:07:31 -0500

    added more stuff to home page, looking better.

--- WorkingWithAngular/src/app/home/home.component.ts
+++ WorkingWithAngular/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsArray } from '../Products';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products = ProductsArray(6);
 
   constructor() { }
 
